3df0348b875af77a45c747a9395ad98c32e70949
Giantrafa <giantrafa@gmail.com>
2025-08-23 00:31:50 +0000

    corrigir ultimo loop

--- Aula_Prog_Imperativa/Exemplo02.java
+++ Aula_Prog_Imperativa/Exemplo02.java
@@ -1,5 +1,28 @@
 public class Exemplo02{
     public static void main (String[]args){
-        System.out.println("hello World");
+
+        for(int i = 1; i <= 100; i++){
+            System.out.println(i);
+        }
+
+        System.out.println("");
+        for(int i = 1; i <=100; i+=2){
+            System.out.println(i);
+        }
+
+        System.out.println("");
+        for(int i = 2; i <= 100; i+=2){
+            System.out.println(i);
+        }
+
+        System.out.println("");
+        for(int i = 3; i <= 100; i+=3){
+            System.out.println(i);
+        }
+
+        // i %3==0 || i %10==3
+        for(int i = 3; i <= 100; i++){
+            System.out.println(i);
+        }
     }
 }
