41c863d6f29a91d9006d63bf26a9c2f481e8c5a6
Giantrafa <giantrafa@gmail.com>
2025-08-27 13:57:39 +0000

    Revisar

--- Aula_Prog_Imperativa/Exemplo02.java
+++ Aula_Prog_Imperativa/Exemplo02.java
@@ -1,27 +1,28 @@
 public class Exemplo02{
     public static void main (String[]args){
-
+        System.out.println("loop de 1 a 100");
         for(int i = 1; i <= 100; i++){
             System.out.println(i);
         }
 
-        System.out.println("");
+        System.out.println("Números impares");
         for(int i = 1; i <=100; i+=2){
             System.out.println(i);
         }
 
-        System.out.println("");
+        System.out.println("Números pares");
         for(int i = 2; i <= 100; i+=2){
             System.out.println(i);
         }
 
-        System.out.println("");
+        System.out.println("Multiplos de 3");
         for(int i = 3; i <= 100; i+=3){
             System.out.println(i);
         }
 
-        // i %3==0 || i %10==3
+        System.out.println("Multiplos de 3 e termina com 3");
         for(int i = 3; i <= 100; i++){
+            if( i%3==0|| i%10==3)
             System.out.println(i);
         }
     }
