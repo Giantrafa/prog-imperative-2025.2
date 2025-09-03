b524a74196409a104b8ce5eb9e446b13c9da9602
Giantrafa <giantrafa@gmail.com>
2025-09-03 19:10:47 +0000

    analizar

--- Aula_Prog_Imperativa/Tarefas/Tarefa01.java
+++ Aula_Prog_Imperativa/Tarefas/Tarefa01.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class TarefaMarcio {
+public class Tarefa01 {
     public static Scanner input = new Scanner(System.in);        
     public static void main (String[]args){
         
