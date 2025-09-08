ad6180953fd72f0b73286567d32bc4c96886e48f
Giantrafa <giantrafa@gmail.com>
2025-09-08 12:49:53 +0000

    Separar os Metodos

--- Aula_Prog_Imperativa/Exemplo06.java
+++ Aula_Prog_Imperativa/Exemplo06.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Exemplo06 {
     public static Scanner input = new Scanner(System.in);
-    // deixar o usuario definir o limite
+    // deixar o usuario definir o limite ?
     public static final  int valorMaximo = 5;
         public static void main(String [] args){
 
@@ -11,38 +11,46 @@ public class Exemplo06 {
             int [] C = new int[valorMaximo];
             
             
-            preencherDigitacao(A,B);
+            System.out.println("Digite os numeros do 1° vetor");
+            preencherDigitacao(A);
+            
+            System.out.println("Digite os numeros do 2° vetor");
+            preencherDigitacao(B);
+            
             somar(A, B, C);
+            
+            System.out.println("Numeros do 1° vetor");
+            impressao(A);
+            System.out.println();
+            System.out.println("Numeros do 1° vetor");
+            impressao(B);
+            
+            System.out.println("Soma dos 2 vetores");
+            impressao(C);
         }
-        //  imrpimir;             
-        //  somar
 
-    public static void preencherDigitacao (int[] A, int[] B){
-        
-        for(int i = 0; i<A.length; i++){
-            System.out.println("digite o "+(i+1)+"° numero");
-            A[i] = input.nextInt();
+
+    public static void preencherDigitacao (int[] V){
+        for(int i = 0; i<V.length; i++){
+            System.out.print("Digite o " + (i + 1) + "º valor: ");
+            V[i] = input.nextInt();
         }       
-        
-        for(int i =0 ; i<B.length; i++){
-            System.out.println("digite o "+(i+6)+"° numero");
-            B[i] = input.nextInt();
-        }
     } 
+
 
     public static void somar (int[]A, int[]B ,int[] C ){
         for (int i = 0; i < C.length; i++) {
-            C[i] = A[i] + B[i];
-            System.out.println();
-            System.out.println(C[i]);
+            C[i]= A[i] + B[i];
         }
-            
+    }       
 
-    
+    public static void impressao (int[] V){
+        if (V.length > 0){
+            System.out.print(" "+V[0]);
+        }
+        for ( int i =1;  i< V.length; i++ ){
+            System.out.print(", " + V[i]);
+        }
     }
-
-
-
-
+    
 }
-
