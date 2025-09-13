192b74626367ccd96335c84cc9119b8eff12f3ab
Giantrafa <giantrafa@gmail.com>
2025-09-13 00:22:53 +0000

    Prova do semestre passado

---
+++ Aula_Prog_Imperativa/Tarefas/TarefaMarcio02.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class TarefaMarcio02 {
+    public static final int QTD = 100;
+    public static void main(String[]args){
+        int [] vetor1 = new int[QTD] ;
+        int [] vetor2 = new int[QTD] ;
+        int [] vetorResult = new int[QTD] ;
+
+
+    }
+
+}
