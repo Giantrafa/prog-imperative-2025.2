cc61be78c03cc610fb3d082e62a8d8c64e130268
Giantrafa <giantrafa@gmail.com>
2025-09-21 12:26:21 +0000

    update 09/21

--- tarefaSala.java
+++ tarefaSala.java
@@ -3,64 +3,179 @@ import java.util.Scanner;
 public class tarefaSala {
     public static Scanner input = new Scanner(System.in);
     public static final int QTD = 30;
-    
+
     public static void main(String[] args){
-        int [] conjuntoA =new int [QTD];
-        int [] conjuntoB =new int [QTD];
-        
-        System.out.println("insira os numeros do 1° vetor");
-        receber(conjuntoA);
-        System.out.println("insira os numeros do 2° vetor");
-        receber(conjuntoB);
+        int[] conjuntoA = new int[QTD];
+        int[] conjuntoB = new int[QTD];
+        int[] conjuntoC = new int[QTD];
 
-        imprimir(conjuntoA);
-        imprimir(conjuntoB);
+        int limi1 = 0; 
+        int limi2 = 0; 
+        int limi3 = 0; 
 
+        int data;
+
+        do {
+            System.out.println("Digite:");
+            System.out.println("1 - Inserir número no conjunto A");
+            System.out.println("2 - Inserir número no conjunto B");
+            System.out.println("3 - Imprimir conjuntos A e B");
+            System.out.println("4 - União de A e B");
+            System.out.println("5 - Interseção de A e B");
+            System.out.println("6 - Diferença A - B");
+            System.out.println("7 - Diferença B - A");
+            System.out.println("0 - Sair");
+
+            data = input.nextInt();
+
+            switch(data) {
+                case 1:
+                    limi1 = receber(conjuntoA, limi1);
+                    break;
+
+                case 2:
+                    limi2 = receber(conjuntoB, limi2);
+                    break;
+
+                case 3:
+                    System.out.println("Conjunto A:");
+                    imprimir(conjuntoA, limi1);
+                    System.out.println("Conjunto B:");
+                    imprimir(conjuntoB, limi2);
+                    break;
+
+                case 4:
+                    limi3 = uniao(conjuntoA, limi1, conjuntoB, limi2, conjuntoC);
+                    System.out.println("União de A e B:");
+                    imprimir(conjuntoC, limi3);
+                    break;
+
+                case 5:
+                    limi3 = interseccao(conjuntoA, limi1, conjuntoB, limi2, conjuntoC);
+                    System.out.println("Interseção entre A e B:");
+                    imprimir(conjuntoC, limi3);
+                    break;
+
+                case 6:
+                    limi3 = diferenca(conjuntoA, limi1, conjuntoB, limi2, conjuntoC);
+                    System.out.println("Diferença A - B:");
+                    imprimir(conjuntoC, limi3);
+                    break;
+
+                case 7:
+                    limi3 = diferenca(conjuntoB, limi2, conjuntoA, limi1, conjuntoC);
+                    System.out.println("Diferença B - A:");
+                    imprimir(conjuntoC, limi3);
+                    break;
+
+                case 0:
+                    System.out.println("Encerrado");
+                    break;
+
+                default:
+                    System.out.println("Opção inválida.");
+            }
+
+        } while (data != 0);
     }
-    // receber apenas 1 elemento por vez, estararia certo ????
-    public static void receber (int[]V){        
-        int limi = 0;
-        while (limi<=QTD) {
-            for(int i = 0;i<=V.length;i++){            
-                V[i] = input.nextInt();
-                limi++;
-                // condiçao de saida if(V[i])
-            }                    
+
+
+
+    public static int receber(int[] V, int limi){
+        if (limi < V.length) {
+            System.out.println("Digite o número do vetor:");
+            V[limi] = input.nextInt();
+            return limi + 1;
+        } else {
+            System.out.println("Vetor está cheio.");
+            return limi;
         }
     }
 
-    public static void imprimir(int[]V){
+    public static void imprimir(int[] V, int tamanho) {
         System.out.print("{ ");
-        if(V.length > 0){
-            System.out.print("V[0]");
-            for(int i=1; i<V.length; i++){
-                System.out.print(", "+V[i]);
-            }
-            System.out.print(" }");
+        for (int i = 0; i < tamanho; i++) {
+            System.out.print(V[i]);
+            if (i < tamanho - 1) System.out.print(", ");
         }
+        System.out.println(" }");
     }
 
-    public static int buscaBinaria (int[]V, int T){
-        int inicio = 0;
-        int fim = V.length-1;
+    public static int uniao(int[] A, int tamA, int[] B, int tamB, int[] C) {
+        int tamC = 0;
 
-        while (inicio < fim){
-            int meio = (inicio + fim)/2;
-            if(meio == T){
-                return meio;
+        for (int i = 0; i < tamA; i++) {
+            C[tamC++] = A[i];
+        }
 
-            }else if(V[meio] < T){
-                inicio = meio + 1;
-            }else{
-                fim = meio -1;
+        for (int i = 0; i < tamB; i++) {
+            boolean duplicado = false;
+            for (int j = 0; j < tamA; j++) {
+                if (B[i] == A[j]) {
+                    duplicado = true;
+                    break;
+                }
+            }
+            if (!duplicado) {
+                C[tamC++] = B[i];
             }
         }
-    return-1;
+
+        return tamC;
     }
 
+    public static int interseccao(int[] A, int tamA, int[] B, int tamB, int[] C) {
+        int tamC = 0;
+
+        for (int i = 0; i < tamA; i++) {
+            for (int j = 0; j < tamB; j++) {
+                if (A[i] == B[j]) {
+                    boolean duplicado = false;
+                    for (int k = 0; k < tamC; k++) {
+                        if (C[k] == A[i]) {
+                            duplicado = true;
+                            break;
+                        }
+                    }
+
+                    if (!duplicado) {
+                        C[tamC++] = A[i];
+                    }
+
+                    break;
+                }
+            }
+        }
+
+        return tamC;
+    }
+    public static int diferenca(int[] A, int tamA, int[] B, int tamB, int[] C) {
+        int tamC = 0;
+        for (int i = 0; i < tamA; i++) {
+            boolean encontrado = false;
+
+            for (int j = 0; j < tamB; j++) {
+                if (A[i] == B[j]) {
+                    encontrado = true;
+                    break;
+                }
+            }
+
+            if (!encontrado) {
+                boolean duplicado = false;
+                for (int k = 0; k < tamC; k++) {
+                    if (C[k] == A[i]) {
+                        duplicado = true;
+                        break;
+                    }
+                }
+                if (!duplicado) {
+                    C[tamC++] = A[i];
+                }
+            }
+        }
+
+        return tamC;
+    }
 
 }
-
-
-
-
