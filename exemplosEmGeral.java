0a0c521994404cb9ad4020d398a82ede3076bc58
Giantrafa <giantrafa@gmail.com>
2025-09-24 19:13:08 +0000

    MaisClaresa

--- exemplosEmGeral.java
+++ exemplosEmGeral.java
@@ -1,109 +1,152 @@
 import java.util.Scanner;
 
 public class exemplosEmGeral {
-      public static final int QTD = 5;
-    public static Scanner input = new Scanner(System.in);
+public static Scanner input = new Scanner(System.in);
+    public static final int QTD = 30;
+
     public static void main(String[]args){
-        int [] vetor1 = new int [QTD];
-        int [] vetor2 = new int [QTD];
-        int [] vetor3 = new int [QTD];
-        
-        int valor = input.nextInt(); //valor do busca binaria
+        int[] vetor1 = new int[QTD];
+        int[] vetor2 = new int[QTD];
+        int[] vetor3 = new int[QTD];
 
-
+        int valor = input.nextInt();
+        int tam = 0;
     }
 
-    //saida da maneira de marcio
-    public static void impressao (int[]V){
-        System.out.print("{");                    
-        if(V.length>0){                
-            System.out.println(" "+V[0]);        
-            for(int i=1; i<=V.length; i++){
-                System.out.print(", "+V[i]);
-            }
+    //Metodo de impressao de marcio
+    public static void impressao(int[]V, int tam){
+        System.out.print("{ ");
+        if(tam>=0){
+            System.out.print(V[0]);
+        }
+        for(int i =1; i<=tam; i++){
+            System.out.print(", "+V[i]);            
+        }
         System.out.print(" }");
+    }
+
+    // Inseri o todos os numeros no vetor de uma vez so e contar quantos foram 
+    public static int receber(int[]V, int tam){
+        for(int i = 0; i<V.length; i++){
+            V[i] = input.nextInt();
+        }
+        return tam ++;
+    }
+
+    // Inseri um numero por vez  e voltar para o menu , do meu jeito 
+    public static int receber2(int[]V, int tam){
+        if (tam < V.length) {
+            V[tam] = input.nextInt();
+            return tam +1;
+        } else {
+            System.out.println("vetor esta cheio");
+            return tam;
+        }                        
+    }
+
+    // Somar 2 vetores para um 3 vetor , onde tam seria o tamanho preenchido dentro dos vetores
+    public static void somar(int[]V, int[]N, int[]M, int tam){
+        for(int i = 0; i<tam; i++){
+            V[i] = N[i] + M[i];
         }
     }
 
-    //armazena os numeros dos vetor
-    public static void armazena (int[]V){
-        for(int i=0; i<=QTD; i++){
-            System.out.println("digite o "+(i+1)+"° numero do vetor");
-            V[i]= input.nextInt();
+    // Pega um vetor e inverte ele para outro 
+    public static void inverso (int[]V, int[]N, int tam){
+        for(int i = 0; i<tam; i++){
+            V[i] =N[tam-i-1];
         }
-    }
+    }    
 
-    // soma de vetores
-    public static void soma(int[]N,int[]M,int[]T){
-        for(int i=0; i<=QTD; i++){
-            T[i]= N[i]+ M[i];
-        } 
-    }
-
-    //pegar o vetor e inverter ele
-    public static void inverso(int[]N,int[]V){
-        for(int i=0; i<N.length; i++){
-            V[i]= N[N.length-i-1];
-        }
-    }
-
-    //filtra os numeros do vetor e adiciona eles a V
-    public static void impares(int[]N,int[]M,int[]V){
+    //Filtra os numeros do vetor e adiciona eles a V para mosra os pares de um vetor e impares do outro
+    public static void impares(int[]N, int[]M, int[]V,int tam){
         int index = 0;
 
-        for(int i=0; i<N.length; i++){
+        for(int i=0; i<tam; i++){
             if(N[i]%2==0){
                 V[index] = N[i];
                 index++;
             }
         }
 
-        for(int i=0; i<M.length; i++){
+        for(int i=0; i<tam; i++){
             if(M[i]%2==1){
                 V[index] = M[i];
                 index++;
             }
+        }                                
+        impressao(V, tam);// me livro de escrever outra impressao e ainda fica da mesma maneira do outro
+        } 
+
+        //  Pego um valor e procuro dentro do vetor usando o meio q vai sempre dividir o array para achar ele ou nao 
+        public static int buscaBinaria(int[]V, int x){
+            int busca = x;
+
+            int inicio = 0;
+            int fim = V.length -1;
+            
+            while( inicio <= fim){
+                int meio = (inicio + fim)/2;
+                if(V[meio]== busca){
+                    return meio;
+                } else if(V[meio] < busca){
+                    inicio = meio +1;
+                }else{
+                    fim = meio -1;
+                }
+            }
+            return -1;
         }
 
-        for(int i=0;i<index;i++){
-            System.out.print(V[i]+" ");
-            //2,4,7,9
-        }
-        
-        System.out.println("");
 
-        for(int i=0;i<V.length;i++){
-            System.out.print(V[i]+" ");
-            //2,4,7,9,0
-        }        
-    }
+    // ou Ordenação por Inserção
+    // Pega os numeros do vetor e organiza eles na ordem cresente
+        public static void insertionSort(int []V, int tam){
+            for(int i=1; i<tam; i++){
+                int chave = V[i];
+                int J = i-1;
 
-    // Retorna o índice onde o valorBusca está localizado no vetor, ou -1 caso o valor não seja encontrado.
-    public static int buscaBinaria(int[]V,int valorBusca){
-        int inicio = 0;
-        int fim = V.length -1;
-
-        while(inicio <= fim){
-            int meio = (inicio+fim)/2;
-            if(V[meio] == valorBusca){
-                return meio;
-            } else if(V[meio] < valorBusca){
-                inicio = meio +1; 
-            }else{
-            fim = meio -1;
+                while(J>=0 && chave<V[J]){
+                    V[J+1] = V[J];
+                    J--;
+                }
+                V[J+1] = chave;
             }
         }
-        return -1;
-    }    
 
+        // So funciona caso os numeros estejam organizados em ordem
+        public static void repetido(int[]V,int tam){
 
-    public static void insertionSort(int[]V){
-        for(int i =1; i<V.length; i++){
+            insertionSort(V,tam);
 
+            int QTD = 1;
+            for(int i=1; i<tam; i++){
+                if(V[i]!=V[i-1]){
+                    System.out.println(V[i-1]+" | "+QTD);
+                    QTD = 1;
+                }else{
+                    QTD++;
+                }
+            }
+            System.out.println(V[V.length-1]+" | "+QTD);
         }
+
+        // conferir
+        public static int remover (int[]V,int tam, int x){
+        int pos = buscaBinaria(V,x);
+        if( pos == -1){
+            System.out.println("nao encontrado o vetor");
+            return tam;
+        }
+        for(int i = pos; i< tam-1;i++){
+            V[i] = V[i+1];
+        }
+        return tam-1;
     }
 
-    public static void repetidos(){
+
+    // passar por cada um dentro do array e retirar o valor dele
+    public static void remover2(int[]V, int X){
 
     }
 
